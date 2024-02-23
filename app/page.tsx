1cc08317dad80a3a667b2df04cb2711d005fd419
zoooesfG <zoe.ferguson@globacore.com>
2024-02-23 12:03:27 -0500

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@
 export default async function Home({ searchParams }: { searchParams: { id: string } }) {
   // const task = await fetchTask("8686nmc8c")
 
-
-
+// By default a file is created but you could switch between Buffer and Streams by using "buffer" or "stream" respectively.
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 w-full">
 
